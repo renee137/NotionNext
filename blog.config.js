a39a58cc2236a2ee4b34eef2b1303f0f3f301274
renee137 <168628926+renee137@users.noreply.github.com>
2024-05-06 23:22:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,16 +28,6 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || 'https:/reneediary.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
-  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: '', // 你的微博个人主页
-  CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB:  '', // 你的github个人主页 例如 https://github.com/tangly1024
-  CONTACT_TELEGRAM: '', // 你的telegram 地址 例如 https://t.me/tangly_1024
-  CONTACT_LINKEDIN: '', // 你的linkedIn 首页
-  CONTACT_INSTAGRAM:'', // 您的instagram地址
-  CONTACT_BILIBILI: '', // B站主页
-  CONTACT_YOUTUBE: '', // Youtube主页
 
   NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
 
